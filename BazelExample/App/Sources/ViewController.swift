584e4784f43d42e4c729d2f5837bd8699abcb0f3
Nguyen Nghia <nghiaklpro195@gmail.com>
2020-04-24 17:27:20 +0700

    first commit

---
+++ BazelExample/App/Sources/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BazelExample
+//
+//  Created by Nguyen Van Nghia on 4/24/20.
+//  Copyright © 2020 Nguyen Van Nghia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+    }
+
+
+}
+
---
+++ BazelExample/Submodule/Networking/Sources/Networking.swift
@@ -0,0 +1,9 @@
+//
+//  Networking.swift
+//  BazelExample
+//
+//  Created by Nguyen Van Nghia on 4/24/20.
+//  Copyright © 2020 Nguyen Van Nghia. All rights reserved.
+//
+
+import Foundation
